21b2ab1b962c2591885937f9711fc34e7459cbb2
Ayman Naif <ayman_naif@hotmail.com>
2021-03-01 17:43:40 +0200

    add new file

---
+++ js/app.js
@@ -0,0 +1,81 @@
+    'use strict';
+
+   
+    confirm("here we goooo , with guessing game !");
+    let MyName=prompt("is my name Ayman ?").toLowerCase();
+    let right="YES".toLowerCase();
+    let correct="Y".toLowerCase();
+     
+    if(MyName==right) {
+      alert("wooohoooo , that's correct my name is Ayman Naif ");
+      
+    } 
+    else if (MyName==correct)
+    { 
+      alert("wooohoooo , that's correct my name is Ayman Naif ");
+    }
+    else{
+      alert("naaah , not good answer !! try again");
+    }
+
+    let age=prompt("is my age 25?").toLowerCase();
+    
+    if(age==right) {
+      alert("wooohoooo , that's correct my name is Ayman Naif ");
+      
+    } 
+    else if (age==correct)
+    {
+      alert("wooohoooo , that's correct my name is Ayman Naif ");
+    }
+    else{
+      alert("naaah , not good answer !! try again");
+    }
+
+    let live=prompt("do I live in Jordan?").toLowerCase();
+     
+    if(live==right) {
+      alert("wooohoooo , that's correct my name is Ayman Naif ");
+      
+    } 
+    else if (live==correct)
+    {
+      alert("wooohoooo , that's correct my name is Ayman Naif ");
+    }
+    else{
+      alert("naaah , not good answer !! try again");
+    }
+
+    let learn=prompt("am I learning software devlopment?").toLowerCase();
+     
+    if(learn==right) {
+      alert("wooohoooo , that's correct my name is Ayman Naif ");
+      
+    } 
+    else if (learn==correct)
+    {
+      alert("wooohoooo , that's correct my name is Ayman Naif ");
+    }
+    else{
+      alert("naaah , not good answer !! try again");
+    }
+    
+    let study=prompt("did I study at Applied Al Balqa'a Univirsity?").toLowerCase();
+     
+    if(study==right) {
+      alert("wooohoooo , that's correct my name is Ayman Naif ");
+      
+    } 
+    else if (study==correct)
+    {
+      alert("wooohoooo , that's correct my name is Ayman Naif ");
+    }
+    else{
+      alert("naaah , not good answer !! try again");
+    }
+    
+          
+
+
+
+
